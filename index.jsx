bab7eb5b90efdbda427613d82449ea307819b9c0
radubrehar <radu@jslog.com>
2014-12-15 10:37:05 +0200

    remove ununsed requires

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
-require('es6-promise').polyfill()
-require('fetch')
+// require('es6-promise').polyfill()
+// require('fetch')
 
 var React = require('react')
 var Combo = require('./src')
